5a8360c76e33908f2ebbce984d5691b52a9f852b
Parth-Hariyani3001 <hariyaniparth-inft@atharvacoe.ac.in>
2024-01-31 22:18:27 +0530

    Changes related to button and text color

--- src/components/Not-Found/NotFound.jsx
+++ src/components/Not-Found/NotFound.jsx
@@ -29,19 +29,18 @@ function NotFound() {
           </header>
 
           <div className="text-center py-10 px-4 sm:px-6 lg:px-8">
-            <h1 className="block text-7xl font-bold text-gray-800 sm:text-9xl dark:text-white">
+            <h1 className="block text-7xl font-bold text-gray-100 sm:text-9xl dark:text-white">
               404
             </h1>
-            <h1 className="block text-2xl font-bold text-white"></h1>
-            <p className="mt-3 text-gray-600 dark:text-gray-400">
+            <p className="mt-3 text-gray-100 dark:text-white">
               Oops, something went wrong.
             </p>
-            <p className="text-gray-600 dark:text-gray-400">
+            <p className="text-gray-100 dark:text-white">
               Sorry, we couldn't find your page.
             </p>
             <div className="mt-5 flex flex-col justify-center items-center gap-2 sm:flex-row sm:gap-3">
               <button
-                className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-green-400 text-white hover:bg-green-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 onClick={redirectToRepo}
               >
                 <svg
@@ -58,11 +57,11 @@ function NotFound() {
               </button>
               <button
                 onClick={redirectToHome}
-                className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                className="w-full sm:w-auto py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-100 dark:hover:text-gray-100 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
                 href="../examples.html"
               >
                 <svg
-                  className="flex-shrink-0 w-4 h-4"
+                  className="flex-shrink-0 w-4 h-4 text-gray-100"
                   xmlns="http://www.w3.org/2000/svg"
                   width="24"
                   height="24"
